7038e5748ae48c00b2a1703e8564524ad89cf9b5
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-07-30 10:50:04 -0300

    feat: criando rota para atualizar local do usuário logado

--- src/controllers/LocalController.js
+++ src/controllers/LocalController.js
@@ -169,7 +169,75 @@ class LocalController {
     }
 
 
-    
+
+    async atualizar(request, response) {
+        try {
+            const id = request.params.local_id
+            const dados = request.body
+            const userId = request.userId;
+
+            const local = await Local.findByPk(id)
+
+            if (!local) {
+                response.status(404).json({ mensagem: 'Não foi encontrado o local' })
+            }
+
+
+            //Validação: garante que apenas o usuário que cadastrou o local possa realizar essa operação
+            if (local.usuarioId != userId) {
+                return response
+                    .status(401)
+                    .json({ mensagem: 'Usuário não autorizado.' })
+            }
+
+
+
+            //Validação latitude
+            if (dados.latitude && !latitudePattern.test(dados.latitude)) {
+                return response
+                    .status(400)
+                    .json({ mensagem: 'A latitude não está no formato válido (eg., -22.95192 ou 38.89769).' });
+            }
+            const latitudeNum = parseFloat(dados.latitude);
+            if (dados.latitude && !(latitudeNum >= -90 && latitudeNum <= 90)) {
+                return response
+                    .status(400)
+                    .json({ mensagem: 'A latitude deve estar entre -90 e 90.' });
+            }
+
+
+
+            //Validação longitude
+            if (dados.longitude && !longitudePattern.test(dados.longitude)) {
+                return response
+                    .status(400)
+                    .json({ mensagem: 'A longitude não está no formato válido (eg., -43.21047 ou 116.57036).' });
+            }
+            const longitudeNum = parseFloat(dados.longitude);
+            if (dados.longitude && !(longitudeNum >= -180 && longitudeNum <= 180)) {
+                return response
+                    .status(400)
+                    .json({ mensagem: 'A longitude deve estar entre -180 e 180.' });
+            }
+
+            
+
+            // Remove campos id e usuarioId do objeto dados para impedir atualização
+            delete dados.id;
+            delete dados.usuarioId;
+
+
+            await local.update(dados);
+
+            response.status(200).json({ mensagem: 'Atualizado com sucesso' })
+
+        } catch (error) {
+            console.log(error)
+            response.
+                status(500).
+                json({ mensagem: 'Houve um erro ao atualizar o local de treino.' })
+        }
+    }
 
 }
 
--- src/routes/locais.routes.js
+++ src/routes/locais.routes.js
@@ -8,6 +8,7 @@ locaisRoutes.post('/', LocalController.criar)
 locaisRoutes.get('/', LocalController.listarTodos)
 locaisRoutes.get('/:local_id', LocalController.listarUm)
 locaisRoutes.delete('/:local_id', LocalController.deletar)
+locaisRoutes.put('/:local_id', LocalController.atualizar)
 
 
 module.exports = locaisRoutes
