8e98dd8aacbe8734f0b0801bf6cdd6895c0749bb
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-07-31 11:35:15 -0300

    feat: documentando rotas usuarios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,7 +9,22 @@ const routes = new Router()
 
 //Rotas Públicas
 routes.use('/usuario', usuariosRoutes)
-routes.use('/login', LoginController.login)
+routes.use('/login', LoginController.login
+    /*
+    #swagger.tags = ['Usuário'],
+    #swagger.description = 'Endpoint para realizar login no sistema',
+    #swagger.parameters['novoLogin'] = {
+        in: 'body',
+        description: 'Informações do login',
+        required: true,
+        schema: { 
+            $email: 'teste@email.com',
+            $password: senha123
+        }
+    },
+ */
+)
+
 
 
 //Rotas Privadas
