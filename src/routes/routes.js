b795fe2ebc80e376f35bb1026ef2a57f13bf1a69
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-07-31 10:28:57 -0300

    feat: configurando inicialmente o swagger

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,21 @@
 const { Router } = require('express')
+
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./doc.swagger.json')
+
+
 const usuariosRoutes = require('./usuarios.routes')
 const LoginController = require('../controllers/LoginController')
 const locaisRoutes = require('./locais.routes')
+
 const validaToken = require('../middlewares/validaToken')
 
 
 const routes = new Router()
+
+
+routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 
 //Rotas Públicas
 routes.use('/usuario', usuariosRoutes)
